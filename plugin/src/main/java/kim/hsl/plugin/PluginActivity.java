478a23532e4c787b11401476b0a64017427759a3
han1202012 <octopus_truth@163.com>
2021-05-30 22:44:50 +0800

    Initial commit

---
+++ plugin/src/main/java/kim/hsl/plugin/PluginActivity.java
@@ -0,0 +1,14 @@
+package kim.hsl.plugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PluginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plugin);
+    }
+}
