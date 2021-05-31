80d81f9e32b9430f90ccd47b1c94d9ee50b51d6c
han1202012 <octopus_truth@163.com>
2021-05-31 21:19:44 +0800

    update

--- app/src/main/java/kim/hsl/plugin/MainActivity.java
+++ app/src/main/java/kim/hsl/plugin/MainActivity.java
@@ -3,12 +3,23 @@ package kim.hsl.plugin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "getClassLoader() : " +
+                getClassLoader());
+        Log.i(TAG, "getClassLoader().getParent() : " +
+                getClassLoader().getParent());
+        Log.i(TAG, "getClassLoader().getParent().getParent() : " +
+                getClassLoader().getParent().getParent());
+
     }
 }
